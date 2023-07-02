5f613accb37732a7ed1fe2cd01f075f23bea58c3
Mustafa-iMac <mustafa-iMac@Mustafas-iMac.local>
2023-07-02 12:46:23 -0700

    Initial setup

---
+++ client/src/Header.jsx
@@ -0,0 +1,89 @@
+import { Link } from "react-router-dom";
+
+export const Header = () => {
+  return (
+    <header className="flex justify-between">
+      <a href="" className="flex items-center gap-1 text-primary">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-10 h-10"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+          />
+        </svg>
+
+        <span className="font-bold text-2xl ml-1">
+          <span className="text-black p-0.5">Musti</span>
+          BNB
+        </span>
+      </a>
+
+      <div className="flex gap-2 border border-gray-300 rounded-full py-3 px-4 shadow-md shadow-gray-300">
+        <div>Anywhere</div>
+        <div className="border-l border-gray-300"></div>
+        <div>Any week</div>
+        <div className="border-l border-gray-300"></div>
+        <div>Any guests</div>
+        <button className="bg-primary text-white p-1.5 rounded-full">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-4 h-4"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+            />
+          </svg>
+        </button>
+      </div>
+
+      <Link
+        to={"/login"}
+        className="flex items-center gap-2 border border-gray-300 rounded-full px-4"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+        <div className="text-gray-800 p-1 rounded-full">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+            />
+          </svg>
+        </div>
+      </Link>
+    </header>
+  );
+};
